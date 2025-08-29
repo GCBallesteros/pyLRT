44f50a8925cd0a627a50457ede6fefe3cbe756d7
Edward Gryspeerdt <e.gryspeerdt@imperial.ac.uk>
2025-08-29 22:26:25 +0100

    Brightness temperature function

--- pyLRT/misc.py
+++ pyLRT/misc.py
@@ -24,3 +24,16 @@ def planck_function(temperature, wavelength=None, frequency=None, wavenumber=Non
     elif wavenumber is not None:
         return ((2*h*c**2*wavenumber**3))*(1/(np.exp(h*c*wavenumber/(kb*temperature))-1))
 
+
+def brightness_temperature(radiance, wavelength=None, frequency=None, wavenumber=None):
+    '''Supply one of wavelength in m, frequency in Hz, wavenumber in m-1'''
+    import scipy.constants
+    h = scipy.constants.h
+    c = scipy.constants.c
+    kb = scipy.constants.Boltzmann
+    if wavelength is not None:
+        return ((h*c)/(kb*wavelength))/(np.log(1+(2*h*c**2)/(radiance*wavelength**5)))
+    elif frequency is not None:
+        return ((h*frequency)/(kb))/(np.log(1+(2*h*frequency**3)/(radiance*c**2)))
+    elif wavenumber is not None:
+        return ((h*c*wavenumber)/(kb))/(np.log(1+(2*h*c*frequency**3)/(radiance)))
